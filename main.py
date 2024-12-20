6a9465000df9f8d43a310374819fe6743d6186c2
Luca Mancilik <luca.mancilik@gmail.com>
2024-12-20 15:02:35 +0100

    refactoring

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ plane_id = p.loadURDF("plane.urdf")
 
 p.changeDynamics(plane_id, -1, lateralFriction=FRICTION)
 
-uld = Uld('H1_Class_3_Instance_45_ULD_0.json')
+uld = Uld('H1_Class_3_Instance_45_ULD_0.json', scaling_factor=0.02)
 uld_id = uld.body.render()
 for item in uld.items:
     item.render()
@@ -25,18 +25,12 @@ for item in uld.items:
 position, orientation = p.getBasePositionAndOrientation(uld_id)
 
 for i in range(240*15):
+    # wait one second before force is applied
     if int(i/240) >= 1:
-        force, _ = calculate_force_and_acceleration(FRICTION, uld.body.mass, [0, 0, 1], int((i-240) / 240), 15, 2)
+        force, _ = calculate_force_and_acceleration(FRICTION, uld.body.mass, [1, 0, 0], int((i-240) / 240), 4, 2)
         p.applyExternalForce(uld_id, -1, force, position, p.WORLD_FRAME)
     move_camera(uld_id)
     p.stepSimulation()
     time.sleep(1/240)
 
-    if i % 240 == 0:
-        uld.body.print_velocity()
-
-uld.body.print_position()
-uld.body.print_velocity()
-
 p.disconnect()
-
--- uld.py
+++ uld.py
@@ -6,27 +6,30 @@ from box import Box
 
 
 class Uld:
-    def __init__(self, file_path, uld_height=0.2, uld_color=[0, 0, 0, 1]):
+    def __init__(self, file_path, uld_height=20, uld_color=None, scaling_factor=1.0):
+        if uld_color is None:
+            uld_color = [0, 0, 0, 1]
+
         with open(file_path) as file:
             json_uld = json.load(file)
 
-        uld_half_extents = np.array([json_uld['properties']["width"] / 100 / 2,
-                                     json_uld['properties']["depth"] / 100 / 2,
-                                     uld_height / 2])
+        # Apply scaling factor to dimensions
+        uld_half_extents = np.array([json_uld['properties']["width"] * scaling_factor / 2,
+                                     json_uld['properties']["depth"] * scaling_factor / 2,
+                                     uld_height * scaling_factor / 2])
         uld_position = np.zeros(3) + uld_half_extents
         self.uld_mass = json_uld['properties']['standardWeight']
-        uld_color = uld_color
-        self.body = Box(uld_half_extents, uld_position, [0, 0, 0], self.uld_mass, uld_color)
+        self.body = Box(uld_half_extents.tolist(), uld_position.tolist(), [0, 0, 0], self.uld_mass, uld_color)
         self.total_weight = json_uld['properties']['standardWeight']
         self.items = []
         for item in json_uld["placedItems"]:
             item_half_extents = np.array([
-                item['shape']["width"] / 100 / 2,
-                item['shape']["depth"] / 100 / 2,
-                item['shape']["height"] / 100 / 2
+                item['shape']["width"] * scaling_factor / 2,
+                item['shape']["depth"] * scaling_factor / 2,
+                item['shape']["height"] * scaling_factor / 2
             ])
             item_start_position = np.array(
-                [item['x'], item['z'], item['y'] + uld_height*100]) / 100 + item_half_extents
+                [item['x'], item['z'], item['y'] + uld_height]) * scaling_factor + item_half_extents
             item_mass = item['weight']
             self.total_weight += item_mass
-            self.items.append(Box(item_half_extents, item_start_position, [0, 0, 0], item_mass, color.create_random()))
+            self.items.append(Box(item_half_extents.tolist(), item_start_position.tolist(), [0, 0, 0], item_mass, color.create_random()))
