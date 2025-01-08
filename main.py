eb9ed84cbbe2ce4f4af54cfdb1ba23197511b6f9
Luca Mancilik <luca.mancilik@gmail.com>
2025-01-08 12:42:32 +0100

    Fix bug in previous commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import multiprocessing
-import pandas as pd
 import time
 import csv
 from simulation.simulation import simulate
@@ -19,25 +18,35 @@ force_direction_vectors = [
 ]
 
 def run_simulation(args):
-    file_path, direction, uld = args
+    file_path, direction, uld, lock = args
+    result = simulate(uld,
+                      duration=4,
+                      max_g_force=0.2,
+                      force_duration=3,
+                      force_direction_vector=direction,
+                      ground_friction=1,
+                      uld_friction=0.5,
+                      item_friction=0.8,
+                      scaling_factor=0.01,
+                      visual_simulation=False,
+                      visualization=False,
+                      num_solver_iterations=200,
+                      sim_time_step=240
+                      )
+    result["filename"] = os.path.basename(file_path)
 
-    return simulate(uld,
-                            duration=4,
-                            max_g_force=0.2,
-                            force_duration=3,
-                            force_direction_vector=direction,
-                            ground_friction=1,
-                            uld_friction=0.5,
-                            item_friction=0.8,
-                            scaling_factor=0.01,
-                            visual_simulation=False,
-                            visualization=False,
-                            num_solver_iterations=200,
-                            sim_time_step=240
-                            )
+    with lock:
+        with open('simulation_results.csv', 'a', newline='') as csvfile:
+            fieldnames = ['filename', 'nfb', 'nfb_static', 'nfb_rel', 'nfb_rel_static', 'fallen_boxes', 'fallen_boxes_static',
+                          'force_direction_vector', 'max_g_force', 'item_friction', 'uld_friction',
+                          'simulation_duration', 'uld_half_extents', 'items']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writerow(result)
+
+    return result
 
 if __name__ == "__main__":
-    max_workers = 3
+    max_workers = 2
     pool = multiprocessing.Pool(processes=max_workers)
     lock = multiprocessing.Manager().Lock()
 
@@ -48,25 +57,21 @@ if __name__ == "__main__":
         if not is_statically_stable(result_file_path):
             continue
         file_path = os.path.join(loading_pattern_directory, filename)
-        uld_dict = extract_data(1, file_path= file_path, scaling_factor= 0.01, uld_height=20)
+        uld_dict = extract_data(1, file_path=file_path, scaling_factor=0.01, uld_height=20)
         uld_list = get_uld_transformations(uld_dict)
         for uld in uld_list:
             for direction in force_direction_vectors:
-                tasks.append((file_path, direction, uld))
-    with open('simulation_results.csv', 'a', newline='') as csvfile:
-        fieldnames = ['nfb', 'nfb_static', 'nfb_rel', 'nfb_rel_static', 'fallen_boxes', 'fallen_boxes_static',
+                tasks.append((file_path, direction, uld, lock))
+
+    with open('simulation_results.csv', 'w', newline='') as csvfile:
+        fieldnames = ['filename', 'nfb', 'nfb_static', 'nfb_rel', 'nfb_rel_static', 'fallen_boxes', 'fallen_boxes_static',
                       'force_direction_vector', 'max_g_force', 'item_friction', 'uld_friction', 'simulation_duration',
                       'uld_half_extents', 'items']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
 
-        for result in pool.imap_unordered(run_simulation, [(task, lock) for task in tasks]):
-            pass
+    for result in pool.imap_unordered(run_simulation, tasks):
+        pass
 
-    results = pool.map(run_simulation, tasks)
     pool.close()
     pool.join()
-
-    df = pd.DataFrame(results)
-    df.head()
-    df.to_csv('simulation_results.csv', index=False, sep=";")
